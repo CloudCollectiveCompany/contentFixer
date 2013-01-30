7f3176cb9b090c3302900d30d60de87ead197f3f
Eran <eran@ccco.us>
2013-01-29 20:30:52 -0500

    jQuery overhaul, still broken though...

--- contentFixer.js
+++ contentFixer.js
@@ -1,48 +1,69 @@
 var contentDiv = '#content';
 
+/* var resizableObjects = [ //TODO: Make objects resizable by adding them into an array. Going to have to do it clumsily for now, I guess...
+	'mainImage' //Only works with IDs!
+];
+
 var rObject = {
-	width: 'width'
+	width: 0,
+	height: 0,
+	report: function(){
+		console.log("width: " + this.width + " | height: " + this.height);
+	}
 };
+
+for( var i=0; i>= resizableObjects.length; i++){
+	console.log(rObject.height);
+	resizableObjects[i] = new rObject();
+	resizableObjects[i].report()
+}*/
 
 function centerContent() {
 	var parentHeight = $(window).height();
 	var childHeight = $(contentDiv).height();
 	var offset = Math.ceil(( parentHeight - childHeight) / 2);
 	$(contentDiv).css('margin-top', offset);
-}
+} 
 
 function resizeContent() {	
+	var parentHeight = $(window).height();
+	var contentHeight = $(contentDiv).height();
 	//var windowHeight = getWindowHeight();
 	//contentElement.style.display = 'static';
-	var imageElement = document.getElementById('homeBox'); //TODO: Seems like a ton of redundancy going on here
-	var logoElement = document.getElementById('homeLogo'); // 	   Really need to clean things up when I know how...
+	var imageElement = $('#mainImage'); //TODO: Seems like a ton of redundancy going on here
+	var logoElement = $('#logoImage'); // 	   Really need to clean things up when I know how...
 	var newImgWidth = 0;
 	var newLogoWidth = 0;
-	while (contentHeight > windowHeight){
-		var imageWidth = imageElement.offsetWidth;
-		var logoWidth = logoElement.offsetWidth;
-		newImgWidth = imageWidth - 100; //Default step is 100, too small of an adjustment didn't seem to do anything.
-		newLogoWidth = logoWidth - 100;
-		imageElement.style.maxWidth = newImgWidth + 'px';
-		logoElement.style.maxWidth = newLogoWidth + 'px';
-		//console.log("Content height was: " + contentHeight); //Just proof that something actually happened.
-		contentHeight = contentElement.offsetHeight;
-		//console.log("Content height is now: " + contentHeight);
+	while (contentHeight > parentHeight){
+		var imageWidth = imageElement.height();
+		var logoWidth = logoElement.height();
+
+		console.log("image width: " + imageWidth + " | logo height: " + logoWidth);
+		
+		newImgWidth = imageWidth - 5; //Default step is 100, too small of an adjustment didn't seem to do anything.
+		newLogoWidth = logoWidth - 5;
+		imageElement.css('max-width', newImgWidth);
+		logoElement.css('max-width', newLogoWidth);
+		
+		//Just proof that something actually happened.
+		console.log("Content height was: " + contentHeight); 
+		contentHeight = $(contentDiv).height();
+		console.log("Content height is now: " + contentHeight);
 	}
 	centerContent(); //Probably bad to call a function from within the same function. I should do something about this...
 }			
 
 function assessDaScreen() {
-	if( $(window).height() > $(contentDiv).height()){
-		centerContent();
+	if( $(window).height() < $(contentDiv).height()){
+		resizeContent();
 	}
 	else { 
-		resizeContent() 
+		centerContent();
 	}
 }
 
 $(document).ready(function() {
-	assessDaScreen()
+	setTimeout("assessDaScreen()",1000);
 });
 
 $(window).resize(function() {
